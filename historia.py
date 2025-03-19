3de9d6423049a2a9cb87e7a2c3087b2c65493126
Lara de França <laramelissadefrancamota@gmail.com>
2025-03-19 10:02:19 -0300

    primeiro commit

---
+++ historia.py
@@ -0,0 +1,6 @@
+cenario=input('Qual o cenário da história?')
+protagonista=input('Quem é o personagem principal?')
+persona_encontrou=input('O que o personagem encontrou?')
+local=input('Onde estava a coisa que ele encontrou?')
+
+print(f'{protagonista} acordou pela manhã e foi caminhar pela {cenario}. chegando lá, encontrou {persona_encontrou}, dentro de {local}')
